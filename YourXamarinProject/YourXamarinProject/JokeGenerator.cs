6fef97543f7fb9de070a2e01a5734c45837b150d
AMajor623Production <h.k.cheung@hotmail.co.uk>
2018-11-28 10:02:18 +0000

    First commit.

---
+++ YourXamarinProject/YourXamarinProject/JokeGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace YourXamarinProject
+{
+    public class JokeGenerator
+    {
+        private readonly string apiUrl = "http://api.icndb.com/jokes/random/";
+        private HttpClient client = new HttpClient();
+
+        public async Task<string> GenerateJoke()
+        {
+            string response = await GetJokeResponseAsync();
+            if (string.IsNullOrWhiteSpace(response))
+            {
+                return "Whoops! Couldn't generate Chuck Norris joke.";
+            }
+
+            string joke = ExtractJoke(response);
+
+            return string.IsNullOrWhiteSpace(joke) ?
+                "Hmm, no jokes available." :
+                joke;
+        }
+
+        private string ExtractJoke(string jsonResponse)
+        {
+            string joke = string.Empty;
+
+            JObject obj = JObject.Parse(jsonResponse);
+
+            if (obj != null && obj["type"] != null)
+            {
+                string successStr = obj["type"].Value<string>();
+
+                if (successStr.ToLower().Equals("success"))
+                {
+                    joke = obj["value"]?["joke"]?.Value<string>() ?? string.Empty;
+                }
+            }
+
+            return joke;
+        }
+
+        private async Task<string> GetJokeResponseAsync()
+        {
+            string jokeResponse = await client.GetStringAsync(apiUrl);
+            return jokeResponse;
+        }
+    }
+}
---
+++ YourXamarinProject/YourXamarinProject/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace YourXamarinProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_ClickedAsync(object sender, EventArgs e)
+        {
+            JokeLabel.FontAttributes = FontAttributes.None;
+
+            JokeGenerator generator = new JokeGenerator();
+            JokeLabel.Text = await generator.GenerateJoke();
+        }
+    }
+}
